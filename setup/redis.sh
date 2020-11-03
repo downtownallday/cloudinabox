37f64d69aefc87439c3629aae428d5687a61ec8c
Downtown Allday <downtownallday@gmail.com>
2020-11-03 16:28:37 -0500

    Another try at getting travis to install redis on bionic

--- setup/redis.sh
+++ setup/redis.sh
@@ -3,9 +3,18 @@
 . /etc/cloudinabox.conf  || die "Could not load /etc/cloudinabox.conf"
 . setup/functions.sh     || exit 1
 
-if [ "$TRAVIS" == "true" ] && systemctl is-active --quiet redis-server
-then
-    say "TRAVIS redis already installed, not installing redis-server with apt"
+if kernel_ipv6_lo_disabled; then
+    # travis on ubuntu 18 does not have ip6 on loopback and that
+    # causes the installation to fail in the package postinstall
+    # script. Deal with that here.
+    say "Installing redis the Travis way"
+    apt-get install redis-server  # ignore error
+    if [ $? -ne 0 ]; then
+        tools/editconf.py -s /etc/redis/redis.conf "bind=127.0.0.1" || \
+            die "could not edit /etc/redis/redis.conf"
+        systemctl start redis-server || \
+            die "cloud not start redis!"
+    fi
     
 else
     say "Installing redis"
@@ -14,7 +23,7 @@ fi
 
 # install redis php packages
 
-php="$REQUIRED_PHP_PACKAGE"
+
 apt_install $php-redis $php-apcu || die "Unable to install redis packages"
 
 # enable redis's local Unix socket (for redis on same server as nextcloud)
